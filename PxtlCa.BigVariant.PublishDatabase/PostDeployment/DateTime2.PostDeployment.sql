653ff17b3479dcc9a0738be082816e364d223d3d
Pxtl <pxtl@hotmail.com>
2016-04-13 01:25:33 -0400

    refactor postdeployment after confirming it is necessary to prevent udf from using datetime instead of datetime2

---
+++ PxtlCa.BigVariant.PublishDatabase/PostDeployment/DateTime2.PostDeployment.sql
@@ -0,0 +1,7 @@
+﻿--SSDT/SQLCLR fails at DateTime2, must redefine f'n to use DateTime2.
+ALTER FUNCTION [dbo].[BigVariantFromDateTime2](@value [datetime2])
+RETURNS [dbo].[BigVariant] WITH EXECUTE AS CALLER
+AS 
+EXTERNAL NAME [PxtlCa.BigVariant.Core].[PxtlCa.BigVariant.Core.UserDefinedFunctions].[BigVariantFromDateTime2]
+
+GO
